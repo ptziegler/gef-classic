79bbb1ef70b65d1690e1f10bd2c2040137b78a2a
Patrick Ziegler <ziegler.patrick@hotmail.de>
2024-05-31 21:29:53 +0200

    Merge branch 'zest 2.0' into master
    Following classes have been marked as deprecated and for removal:
    
    org.eclipse.zest.core.ConstraintAdapter
    org.eclipse.zest.layouts.Filter
    org.eclipse.zest.layouts.InvalidLayoutConfiguration
    org.eclipse.zest.layouts.LayoutBendPoint
    org.eclipse.zest.layouts.LayoutEntity
    org.eclipse.zest.layouts.LayoutGraph
    org.eclipse.zest.layouts.LayoutItem
    org.eclipse.zest.layouts.LayoutIterationEvent
    org.eclipse.zest.layouts.LayoutRelationship
    org.eclipse.zest.layouts.LayoutStyles
    org.eclipse.zest.layouts.NestedLayoutEntity
    org.eclipse.zest.layouts.Stoppable
    org.eclipse.zest.layouts.algorithms.ContinuousLayoutAlgorithm
    org.eclipse.zest.layouts.algorithms.HorizontalShift
    org.eclipse.zest.layouts.algorithms.internal.CycleChecker
    org.eclipse.zest.layouts.algorithms.internal.DynamicScreen
    org.eclipse.zest.layouts.constraints.BasicEdgeConstraint
    org.eclipse.zest.layouts.constraints.BasicEntityConstraint
    org.eclipse.zest.layouts.constraints.EntityPriorityConstraint
    org.eclipse.zest.layouts.constraints.LabelLayoutConstraint
    org.eclipse.zest.layouts.constraints.LayoutConstraint
    org.eclipse.zest.layouts.dataStructures.BendPoint
    org.eclipse.zest.layouts.dataStructures.InternalNode
    org.eclipse.zest.layouts.dataStructures.InternalRelationship
    org.eclipse.zest.layouts.progress.ProgressEvent
    org.eclipse.zest.layouts.progress.ProgressListener

--- org.eclipse.zest.core/src/org/eclipse/zest/core/viewers/AbstractZoomableViewer.java
+++ org.eclipse.zest.core/src/org/eclipse/zest/core/viewers/AbstractZoomableViewer.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
- * Copyright 2005-2006, CHISEL Group, University of Victoria, Victoria, BC,
- *                      Canada.
+ * Copyright 2005-2010, 2024 CHISEL Group, University of Victoria, Victoria,
+ *                           BC, Canada.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -22,6 +22,8 @@ import org.eclipse.draw2d.geometry.Rectangle;
  * subclassed by clients.
  *
  * @author Del Myers
+ *
+ * @noextend This class is not intended to be subclassed by clients.
  *
  */
 //@tag bug.156286-Zooming.fix
